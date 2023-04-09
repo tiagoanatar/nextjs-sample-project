f825da192be04d8b552fc426c1d272f928160882
tiagoanatar <tiagoanatar@gmail.com>
2023-04-09 17:05:19 -0300

    Fix provider type

--- app/GlobalRedux/provider.tsx
+++ app/GlobalRedux/provider.tsx
@@ -1,9 +1,10 @@
 'use client';
 
+import { ReactNode } from 'react';
 import { Provider } from 'react-redux';
 import { store } from './store';
 
-export function Providers({ children }) {
+export function Providers({ children } : { children: ReactNode }) {
     return (
         <Provider store={store}>
             {children}
